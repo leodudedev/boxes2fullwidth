da5d51626a555ad9ea6bc4a1d29f335c2bef2bff
leodudedev <leodudedev@gmail.com>
2013-03-13 11:48:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# [jQuery box2fullWidth]
+#### Animated repositioning of the boxes on the page grid
+
+jQuery box2fullWidth is a plugin for repositioning boxs in grid layout whit transitions in jQuery.
+
+
+Usage
+-----
+
+Just include this script after jQuery. Requires jQuery 1.5+.
+
+``` html
+<script src='jquery.js'></script>
+<script src='jquery.box2fullWidth.js'></script>
+
+```
+
+Demo
+---------------
+
+https://googledrive.com/host/0B8uoQ-Ue05dOWVR3OXdwc0x5Z3c/demo.html
+
---
+++ jquery.box2fullWidth.js
@@ -0,0 +1,88 @@
+/*!
+ * jQuery Box to Full Width - Animated repositioning of the boxes on the page grid
+ * Leonardo Pinori <leodudedev@gmail.com>
+ * MIT Licensed.
+ *
+ * http://github.com/leodudedev/
+ */
+(function($){
+	$.fn.box2fullWidth=function(options){
+	
+		var settings = $.extend({
+			minwidth:400,
+			who:'div',
+			margin:5,
+			duration:500,
+			boxdelay:0,
+			positionComplete:function(){}
+		},options);
+
+		return this.each(function(i,e){
+			var vecRow=new Array();
+			var _this=e;
+			var _timeout=null;
+			var fixwh=200;
+			function anim(t,cent,cord,index){
+				if(cord==undefined||cord==''||cord==null){t.remove(); return;}
+				var cx=(((fixwh+settings.margin)*cord.c)+settings.margin+cent);
+				var cy=settings.margin;
+				if(cord.r>0){
+					cy=$(vecRow[cord.r-1][cord.c]).position().top+$(vecRow[cord.r-1][cord.c]).height()+settings.margin;
+					cy=cord.yt+settings.margin;
+				}
+				t.removeClass('b2fw_moved').stop().delay(settings.boxdelay*index).animate({left:cx,top:cy,opacity:1},settings.duration,function(){
+					$(this).addClass('b2fw_moved');
+					if($('.b2fw_moved').length==$(settings.who,_this).length){
+						settings.positionComplete();
+					}
+				});
+			}
+			function getFree(p,vecRow){
+				for(var i=0; i< vecRow.length ;i++){
+					for(var u=0; u<vecRow[i].length;u++){
+						if(vecRow[i][u]=='x'){
+							vecRow[i][u]=p;
+							var y=0;
+							if(i>0){
+								for(var t=(i-1);t>=0;t--){
+									y+=$(vecRow[t][u]).height()+settings.margin;
+								}
+							}
+							return new Object({r:i,c:u,yt:y});
+						}
+					}
+				}	
+			}
+			
+			function repos(){
+				var tgtw=$(_this).width(), addmin=0;
+				if($(_this).width()<settings.minwidth){
+					tgtw=settings.minwidth;
+					addmin=parseInt((settings.minwidth-$(_this).width())/2);
+				}
+				var tile=parseInt(tgtw/(fixwh+settings.margin));
+				var cent=((($(_this).width()-tile*(fixwh+settings.margin))-settings.margin)/2)+addmin;
+				vecRow=new Array();
+				for(var i=0; i<=parseInt($(settings.who,_this).length/tile)+20;i++){
+					var vecCol=new Array();
+					for(var u=1; u<=tile;u++){vecCol.push('x');}
+					vecRow.push(vecCol);
+				}
+				$(settings.who,_this).each(function(i,e){
+					anim($(this),cent,getFree($(this),vecRow),i)
+				});
+			}
+			$(window).resize(function(){
+				clearTimeout(_timeout);
+				_timeout=setTimeout(function(){repos();},200);
+			});
+			$(settings.who,_this).each(function(i,e){
+				$(e).css('position','absolute');
+			});
+			repos();
+		});
+	};
+})(jQuery);
+
+
+
